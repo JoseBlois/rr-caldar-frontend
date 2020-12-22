66630b7c0ef04f21cbae8636d169600252af2d20
jose blois <joseblois3@gmail.com>
2020-12-22 15:01:49 -0300

    feature-94: fixed typo on input atribute

--- src/components/BuildingApp/AddBuilding/index.js
+++ src/components/BuildingApp/AddBuilding/index.js
@@ -36,49 +36,47 @@ export default function AddBuilding(props) {
   };
 
   return (
-    <div>
-      <div className={styles.addFormContainer}>
-        <h2>Add building panel</h2>
-        <form className={styles.addForm} onSubmit={submition}>
-          <div className={styles.formGroup}>
-            <label htmlFor="name">
-              Building Name
-              <input value={building.name} onChange={changeValue} name="name" pe="text" required />
-            </label>
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="address">
-              Building Address
-              <input value={building.address} onChange={changeValue} name="address" type="text" required />
-            </label>
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="company">
-              Building Company
-              <input value={building.company} onChange={changeValue} name="company" type="text" required />
-            </label>
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="boiler1">
-              Building Boiler 1
-              <input value={building.boiler1} onChange={changeValue} name="boiler1" type="text" required />
-            </label>
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="boiler2">
-              Building Boiler 2
-              <input value={building.boiler2} onChange={changeValue} name="boiler2" type="text" />
-            </label>
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="boiler3">
-              Building Boiler 3
-              <input value={building.boiler3} onChange={changeValue} name="boiler3" type="text" />
-            </label>
-          </div>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
+    <div className={styles.addFormContainer}>
+      <h2>Add building panel</h2>
+      <form className={styles.addForm} onSubmit={submition}>
+        <div className={styles.formGroup}>
+          <label htmlFor="name">
+            Building Name
+            <input value={building.name} onChange={changeValue} name="name" type="text" required />
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="address">
+            Building Address
+            <input value={building.address} onChange={changeValue} name="address" type="text" required />
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="company">
+            Building Company
+            <input value={building.company} onChange={changeValue} name="company" type="text" required />
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="boiler1">
+            Building Boiler 1
+            <input value={building.boiler1} onChange={changeValue} name="boiler1" type="text" required />
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="boiler2">
+            Building Boiler 2
+            <input value={building.boiler2} onChange={changeValue} name="boiler2" type="text" />
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="boiler3">
+            Building Boiler 3
+            <input value={building.boiler3} onChange={changeValue} name="boiler3" type="text" />
+          </label>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
