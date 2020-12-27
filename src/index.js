7935f681c13f48d4daa28b1b3d9eacf105158a71
facundo cordoba <facucor.15@gmail.com>
2020-12-27 13:02:04 -0300

    feature 105: implement redux, first structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
