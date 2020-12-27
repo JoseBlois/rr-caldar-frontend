9ecd9166015696c578e8ac2a8c7d2301bb07ae8b
facundo cordoba <facucor.15@gmail.com>
2020-12-27 00:32:38 -0300

    feature 105: implement redux, first structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './redux/reducers/rootReducer';
+
+const configureStore = () => {
+  const enhancer = composeWithDevTools();
+  return createStore(rootReducer, enhancer);
+};
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
