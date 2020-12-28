e37bd6c791a9ab63f5ca6b71657e283d674811bc
jose blois <joseblois3@gmail.com>
2020-12-28 13:59:46 -0300

    feature-101: first API call to get buildings

--- src/redux/reducers/buildingsReducer.js
+++ src/redux/reducers/buildingsReducer.js
@@ -1,41 +1,58 @@
 import {
   GET_BUILDINGS_FETCHING,
-  GET_BUILDINGS_RESOLVED,
+  GET_BUILDINGS_FULFILLED,
   GET_BUILDINGS_REJECTED,
   ADD_BUILDINGS_FETCHING,
-  ADD_BUILDINGS_RESOLVED,
+  ADD_BUILDINGS_FULFILLED,
   ADD_BUILDINGS_REJECTED,
   UPDATE_BUILDINGS_FETCHING,
-  UPDATE_BUILDINGS_RESOLVED,
+  UPDATE_BUILDINGS_FULFILLED,
   UPDATE_BUILDINGS_REJECTED,
   DELETE_BUILDINGS_FETCHING,
-  DELETE_BUILDINGS_RESOLVED,
+  DELETE_BUILDINGS_FULFILLED,
   DELETE_BUILDINGS_REJECTED,
 } from '../types/buildingsTypes';
 
-const buildingReducer = (state = [], action) => {
+const initialState = {
+  loading: false,
+  list: [],
+  error: false,
+};
+
+const buildingReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_BUILDINGS_FETCHING:
-      return [1];
-    case GET_BUILDINGS_RESOLVED:
-      return [2];
+      return {
+        ...state,
+        loading: true,
+      };
+    case GET_BUILDINGS_FULFILLED:
+      return {
+        ...state,
+        loading: false,
+        list: action.payload,
+      };
     case GET_BUILDINGS_REJECTED:
-      return [3];
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
     case ADD_BUILDINGS_FETCHING:
       return [4];
-    case ADD_BUILDINGS_RESOLVED:
+    case ADD_BUILDINGS_FULFILLED:
       return [5];
     case ADD_BUILDINGS_REJECTED:
       return [6];
     case UPDATE_BUILDINGS_FETCHING:
       return [7];
-    case UPDATE_BUILDINGS_RESOLVED:
+    case UPDATE_BUILDINGS_FULFILLED:
       return [8];
     case UPDATE_BUILDINGS_REJECTED:
       return [9];
     case DELETE_BUILDINGS_FETCHING:
       return [10];
-    case DELETE_BUILDINGS_RESOLVED:
+    case DELETE_BUILDINGS_FULFILLED:
       return [11];
     case DELETE_BUILDINGS_REJECTED:
       return [12];
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import boilersReducer from './boilersReducer';
+import buildingReducer from './buildingsReducer';
 
 const rootReducer = combineReducers({
   boilers: boilersReducer,
+  buildings: buildingReducer,
 });
 
 export default rootReducer;
