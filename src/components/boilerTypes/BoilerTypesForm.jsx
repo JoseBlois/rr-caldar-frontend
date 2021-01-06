8e337b5bffbddc74fb07cc9466e668ff82362080
facundo cordoba <facucor.15@gmail.com>
2021-01-05 23:47:13 -0300

    feature 111: form applied

--- src/components/boilerTypes/BoilerTypesForm.jsx
+++ src/components/boilerTypes/BoilerTypesForm.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useState } from 'react';
+import { Form, Field } from 'react-final-form';
 import PropTypes from 'prop-types';
 import Button from '../sharedComponents/Button';
 import styles from './BoilerTypesForm.module.css';
@@ -20,26 +22,53 @@ const BoilerTypesForm = ({
     });
   };
 
-  const submit = () => {
+  const btSubmit = (value) => {
+    console.log(value);
     const boilerTypeToSub = {
-      description: state.description,
+      description: value.description,
     };
     onSubmit(boilerTypeToSub, state.id);
   };
 
   return (
     <div>
-      <form className={styles.boilerTypesFormContainer}>
-        <div className={styles.inputContainer}>
-          <label htmlFor="description">Description</label>
-          <input type="text" id="description" name="description" value={state.description} onChange={onChangeInput} />
-        </div>
-        <div className={styles.buttonContainer}>
-          <Button btnLabel="Cancel" onClick={onClose} />
-          <Button btnLabel="Submit" primary onClick={() => submit()} />
-        </div>
-      </form>
+      <Form
+        onSubmit={btSubmit}
+        initialValues={{ description: '' }}
+        render={({
+          handleSubmit,
+          form,
+        }) => (
+          <form onSubmit={handleSubmit} className={styles.boilerTypesFormContainer}>
+            <div>
+              <label htmlFor="description">Description</label>
+              <Field
+                name="description"
+                component="input"
+                type="text"
+              />
+            </div>
+            <div className={styles.buttonContainer}>
+              <Button btnLabel="Cancel" onClick={onClose} />
+              <Button type="submit" btnLabel="Submit" primary />
+            </div>
+          </form>
+        )}
+      />
     </div>
+    // <div>
+    //   <form className={styles.boilerTypesFormContainer}>
+    //     <div className={styles.inputContainer}>
+    //       <label htmlFor="description">Description</label>
+    //       <input type="text" id="description" name="description"
+    // value={state.description} onChange={onChangeInput} />
+    //     </div>
+    //     <div className={styles.buttonContainer}>
+    //       <Button btnLabel="Cancel" onClick={onClose} />
+    //       <Button btnLabel="Submit" primary onClick={() => submit()} />
+    //     </div>
+    //   </form>
+    // </div>
   );
 };
 
--- src/components/sharedComponents/Button/index.js
+++ src/components/sharedComponents/Button/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './button.module.css';
 
-const Button = ({
-  btnLabel,
-  onClick,
-  primary,
-}) => (
-  <button type="button" className={primary ? styles.buttonPrimary : styles.button} onClick={onClick}>
-    {btnLabel}
-  </button>
-);
+const Button = (props) => {
+  const { primary, onClick, btnLabel } = props;
+
+  return (
+    // eslint-disable-next-line react/button-has-type
+    <button
+    // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+      className={primary ? styles.buttonPrimary : styles.button}
+      onClick={onClick}
+    >
+      {btnLabel}
+    </button>
+  );
+};
 
 Button.defaultProps = {
   primary: false,
