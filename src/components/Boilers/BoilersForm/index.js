91ded363e8c976765410eb2e17613566eba6a54e
Valentín Silvestri <valentinsilvestri@outlook.com>
2021-01-13 22:57:08 -0300

    final form added

--- src/components/Boilers/BoilersForm/index.js
+++ src/components/Boilers/BoilersForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Field } from 'react-final-form';
 import Select from 'react-select';
@@ -10,73 +10,111 @@ const BoilersForm = ({
   onClose,
   boiler,
 }) => {
-  const [state, setState] = useState({
-    description: boiler.description || '',
-    boilerType: boiler.boilerType || '5fcc1d06998cd913c71c7e01',
-    hourMaintenanceCost: boiler.hourMaintenanceCost || 0,
-    hourEventualCost: boiler.hourEventualCost || 0,
-    maintenanceRate: boiler.maintenanceRate || 0,
-    id: boiler._id,
-  });
+  const [boilerTypeOptions, setBoilerTypeOptions] = useState([]);
+  const [pickedBoilerType, setPickedBoilerType] = useState(null);
 
-  const onChangeInput = (e) => {
-    setState({
-      ...state,
-      [e.target.name]: e.target.value,
-    });
-  };
+  useEffect(async () => {
+    const boilerTypesResponse = await fetch('https://caldar-application.herokuapp.com/boilerTypes');
+    const boilerTypesData = await boilerTypesResponse.json();
+    const allBoilerTypes = boilerTypesData.map((boilerType) => ({
+      label: boilerType.description,
+      value: boilerType._id,
+    }));
+    setBoilerTypeOptions(allBoilerTypes);
+    if (boiler) {
+      allBoilerTypes.map((boilerType) => {
+        if (boilerType.value === boiler.boilerType) {
+          setPickedBoilerType(boilerType);
+        }
+        return false;
+      });
+    }
+  }, []);
 
-  const ReactSelectAdapter = (boilerTypesOptions) => (
-    <Select {...boilerTypesOptions} />
-  );
-
-  const submit = () => {
+  const submit = (values) => {
     const boilerToSub = {
-      description: state.description,
-      boilerType: state.boilerType,
-      hourMaintenanceCost: state.hourMaintenanceCost,
-      hourEventualCost: state.hourEventualCost,
-      maintenanceRate: state.maintenanceRate,
+      description: values.description,
+      boilerType: values.boilerType.value,
+      hourMaintenanceCost: values.hourMaintenanceCost,
+      hourEventualCost: values.hourEventualCost,
+      maintenanceRate: values.maintenanceRate,
     };
-    onSubmit(boilerToSub, state.id);
+    onSubmit(boilerToSub, boiler._id);
   };
+
+  const required = (value) => (value ? undefined : 'Required');
 
   return (
     <div>
       <Form
         onSubmit={submit}
         initialValues={{
-          description: state.description,
-          boilerType: state.boilerType,
-          hourMaintenanceCost: state.hourMaintenanceCost,
-          hourEventualCost: state.hourEventualCost,
-          maintenanceRate: state.maintenanceRate,
+          description: boiler.description || 'Boiler',
+          boilerType: pickedBoilerType,
+          hourMaintenanceCost: boiler.hourMaintenanceCost || 0,
+          hourEventualCost: boiler.hourEventualCost || 0,
+          maintenanceRate: boiler.maintenanceRate || 0,
         }}
         render={({ handleSubmit }) => (
           <form className={styles.boilersFormContainer}>
             <div className={styles.inputContainer}>
               <label htmlFor="description">Description:</label>
-              <Field name="description" component="input" type="text" placeholder="Description" value={state.description} onChange={onChangeInput} />
+              <Field name="description" validate={required}>
+                {({ input, meta }) => (
+                  <div>
+                    <input {...input} type="text" placeholder="Boiler ..." />
+                    {meta.error && meta.touched && <div>{meta.error}</div>}
+                  </div>
+                )}
+              </Field>
             </div>
             <div className={styles.inputContainer}>
               <label htmlFor="boilerType">Boiler Type:</label>
-              <Field name="boilerType" component={ReactSelectAdapter} type="text" placeholder="Boiler Types Description" value={state.boilerType} onChange={onChangeInput} />
+              <Field name="boilerType" validate={required}>
+                {({ input, meta }) => (
+                  <div>
+                    <Select {...input} options={boilerTypeOptions} />
+                    {meta.error && meta.touched && <div>{meta.error}</div>}
+                  </div>
+                )}
+              </Field>
             </div>
             <div className={styles.inputContainer}>
               <label htmlFor="hourMaintenanceCost">Hour Maintenance Cost:</label>
-              <Field name="hourMaintenanceCost" component="input" type="number" placeholder="Hour Maintenance Cost" value={state.hourMaintenanceCost} onChange={onChangeInput} />
+              <Field name="hourMaintenanceCost" validate={required}>
+                {({ input, meta }) => (
+                  <div>
+                    <input {...input} type="number" placeholder="0" />
+                    {meta.error && meta.touched && <div>{meta.error}</div>}
+                  </div>
+                )}
+              </Field>
             </div>
             <div className={styles.inputContainer}>
               <label htmlFor="hourEventualCost">Hour Eventual Cost:</label>
-              <Field name="hourEventualCost" component="input" type="number" placeholder="Hour Eventual Cost" value={state.hourEventualCost} onChange={onChangeInput} />
+              <Field name="hourEventualCost" validate={required}>
+                {({ input, meta }) => (
+                  <div>
+                    <input {...input} type="number" placeholder="0" />
+                    {meta.error && meta.touched && <div>{meta.error}</div>}
+                  </div>
+                )}
+              </Field>
             </div>
             <div className={styles.inputContainer}>
               <label htmlFor="maintenanceRate">Maintenance Rate:</label>
-              <Field name="maintenanceRate" component="input" type="number" placeholder="Maintenance Rate" value={state.maintenanceRate} onChange={onChangeInput} />
+              <Field name="maintenanceRate" validate={required}>
+                {({ input, meta }) => (
+                  <div>
+                    <input {...input} type="number" placeholder="0" />
+                    {meta.error && meta.touched && <div>{meta.error}</div>}
+                  </div>
+                )}
+              </Field>
             </div>
             <div className={styles.buttonContainer}>
               <Button btnLabel="Cancel" onClick={onClose} />
-              <Button btnLabel="Submit" primary onClick={() => submit()} />
+              <Button btnLabel="Submit" primary onClick={handleSubmit} />
             </div>
           </form>
         )}
