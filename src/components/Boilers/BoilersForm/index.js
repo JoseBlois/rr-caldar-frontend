b209b413ee29fe50bded7891e50c1d0ea816f215
Valentín Silvestri <valentinsilvestri@outlook.com>
2021-01-14 23:27:45 -0300

    changes on methodology

--- src/components/Boilers/BoilersForm/index.js
+++ src/components/Boilers/BoilersForm/index.js
@@ -1,34 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Form, Field } from 'react-final-form';
-import Select from 'react-select';
+import { bindActionCreators } from 'redux';
 import Button from '../../sharedComponents/Button';
 import styles from './boilersForm.module.css';
+import TextInput from '../../sharedComponents/TextInput';
+import SelectInput from '../../sharedComponents/Select';
+import { required, numberHour, composeValidators } from '../../../utils/validations';
+import { getBoilerTypes as getBoilerTypesAction } from '../../../redux/actions/boilerTypesAction';
+import { getFormatedBoilerTypes } from '../../../redux/selectors/boilerTypesSelectors';
 
 const BoilersForm = ({
   onSubmit,
   onClose,
   boiler,
+  getBoilerTypes,
+  boilerTypes,
 }) => {
-  const [boilerTypeOptions, setBoilerTypeOptions] = useState([]);
-  const [pickedBoilerType, setPickedBoilerType] = useState(null);
-
-  useEffect(async () => {
-    const boilerTypesResponse = await fetch('https://caldar-application.herokuapp.com/boilerTypes');
-    const boilerTypesData = await boilerTypesResponse.json();
-    const allBoilerTypes = boilerTypesData.map((boilerType) => ({
-      label: boilerType.description,
-      value: boilerType._id,
-    }));
-    setBoilerTypeOptions(allBoilerTypes);
-    if (boiler) {
-      allBoilerTypes.map((boilerType) => {
-        if (boilerType.value === boiler.boilerType) {
-          setPickedBoilerType(boilerType);
-        }
-        return false;
-      });
-    }
+  useEffect(() => {
+    getBoilerTypes();
   }, []);
 
   const submit = (values) => {
@@ -42,79 +33,86 @@ const BoilersForm = ({
     onSubmit(boilerToSub, boiler._id);
   };
 
-  const required = (value) => (value ? undefined : 'Required');
-
   return (
     <div>
       <Form
         onSubmit={submit}
         initialValues={{
           description: boiler.description || 'Boiler',
-          boilerType: pickedBoilerType,
-          hourMaintenanceCost: boiler.hourMaintenanceCost || 0,
-          hourEventualCost: boiler.hourEventualCost || 0,
-          maintenanceRate: boiler.maintenanceRate || 0,
+          boilerType: boiler && boiler.boilerTypes ? boilerTypes.filter(
+            (boilerType) => boiler.boilerTypes.includes(boilerType.value),
+          ) : [],
+          hourMaintenanceCost: boiler.hourMaintenanceCost || 1,
+          hourEventualCost: boiler.hourEventualCost || 1,
+          maintenanceRate: boiler.maintenanceRate || 1,
         }}
-        render={({ handleSubmit }) => (
-          <form className={styles.boilersFormContainer}>
+        render={({
+          handleSubmit, submitting, pristine,
+        }) => (
+          <form className={styles.boilersFormContainer} onSubmit={handleSubmit}>
             <div className={styles.inputContainer}>
-              <label htmlFor="description">Description:</label>
-              <Field name="description" validate={required}>
-                {({ input, meta }) => (
-                  <div>
-                    <input {...input} type="text" placeholder="Boiler ..." />
-                    {meta.error && meta.touched && <div>{meta.error}</div>}
-                  </div>
-                )}
-              </Field>
+              <Field
+                name="description"
+                placeholder="Boiler"
+                label="Description: "
+                type="text"
+                validate={required}
+                component={TextInput}
+              />
+            </div>
+            <div className={styles.boilersFormContainer}>
+              <Field
+                name="boilerType"
+                label="Boiler Type: "
+                type="text"
+                component={SelectInput}
+                options={boilerTypes}
+                validate={required}
+              />
             </div>
             <div className={styles.inputContainer}>
-              <label htmlFor="boilerType">Boiler Type:</label>
-              <Field name="boilerType" validate={required}>
-                {({ input, meta }) => (
-                  <div>
-                    <Select {...input} options={boilerTypeOptions} />
-                    {meta.error && meta.touched && <div>{meta.error}</div>}
-                  </div>
-                )}
-              </Field>
+              <Field
+                name="hourMaintenanceCost"
+                placeholder="0"
+                type="text"
+                label="Hour Maintenance Cost: "
+                validate={composeValidators(numberHour, required)}
+                component={TextInput}
+              />
             </div>
             <div className={styles.inputContainer}>
-              <label htmlFor="hourMaintenanceCost">Hour Maintenance Cost:</label>
-              <Field name="hourMaintenanceCost" validate={required}>
-                {({ input, meta }) => (
-                  <div>
-                    <input {...input} type="number" placeholder="0" />
-                    {meta.error && meta.touched && <div>{meta.error}</div>}
-                  </div>
-                )}
-              </Field>
+              <Field
+                name="hourEventualCost"
+                placeholder="0"
+                type="text"
+                label="Hour Eventual Cost: "
+                validate={composeValidators(numberHour, required)}
+                component={TextInput}
+              />
             </div>
             <div className={styles.inputContainer}>
-              <label htmlFor="hourEventualCost">Hour Eventual Cost:</label>
-              <Field name="hourEventualCost" validate={required}>
-                {({ input, meta }) => (
-                  <div>
-                    <input {...input} type="number" placeholder="0" />
-                    {meta.error && meta.touched && <div>{meta.error}</div>}
-                  </div>
-                )}
-              </Field>
-            </div>
-            <div className={styles.inputContainer}>
-              <label htmlFor="maintenanceRate">Maintenance Rate:</label>
-              <Field name="maintenanceRate" validate={required}>
-                {({ input, meta }) => (
-                  <div>
-                    <input {...input} type="number" placeholder="0" />
-                    {meta.error && meta.touched && <div>{meta.error}</div>}
-                  </div>
-                )}
-              </Field>
+              <Field
+                name="maintenanceRate"
+                placeholder="0"
+                type="text"
+                label="Maintenance Rate: "
+                validate={composeValidators(numberHour, required)}
+                component={TextInput}
+              />
             </div>
             <div className={styles.buttonContainer}>
-              <Button btnLabel="Cancel" onClick={onClose} />
-              <Button btnLabel="Submit" primary onClick={handleSubmit} />
+              <Button
+                primary
+                btnLabel="Submit"
+                type="submit"
+                disabled={submitting || pristine}
+                onClick={handleSubmit}
+              />
+              <Button
+                btnLabel="Cancel"
+                type="button"
+                onClick={onClose}
+              />
             </div>
           </form>
         )}
@@ -131,6 +129,16 @@ BoilersForm.propTypes = {
   onClose: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
   boiler: PropTypes.object,
+  getBoilerTypes: PropTypes.func.isRequired,
+  boilerTypes: PropTypes.array.isRequired,
 };
 
-export default BoilersForm;
+const mapStateToProps = (state) => ({
+  boilerTypes: getFormatedBoilerTypes(state),
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  getBoilerTypes: getBoilerTypesAction,
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(BoilersForm);
